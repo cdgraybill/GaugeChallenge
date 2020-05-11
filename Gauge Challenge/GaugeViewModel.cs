0f2c58bb18700ad4edcf801824284916be48af4d
Cameron Graybill <cdgraybill@gmail.com>
2020-05-10 20:41:23 -0500

    Add project files.

---
+++ Gauge Challenge/GaugeViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gauge_Challenge
+{
+    public class GaugeViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+        public GaugeViewModel()
+        {
+
+        }
+
+        int _angle;
+        public int Angle
+        {
+            get
+            {
+                return _angle;
+            }
+
+            set
+            {
+                _angle = value;
+                NotifyPropertyChanged("Angle");
+            }
+        }
+
+        int _value;
+        public int Value
+        {
+            get
+            {
+                return _value;
+            }
+
+            set
+            {
+                if (value > 0 && value < 250)
+                {
+                    _value = value;
+                    Angle = value - 120;
+                    NotifyPropertyChanged("Angle");
+                }
+            }
+        }
+    }
+}
---
+++ Gauge Challenge/Needle.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gauge_Challenge
+{
+    /// <summary>
+    /// Interaction logic for Needle.xaml
+    /// </summary>
+    public partial class Needle : UserControl
+    {
+        public Needle()
+        {
+            InitializeComponent();
+            this.DataContext = new GaugeViewModel();
+        }
+    }
+}
